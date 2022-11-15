96bf75cba9992a4a9fb5f6057c57d236b726a0d3
yusuf <yusufay1157@gmail.com>
2022-11-15 16:42:18 +0300

    Add Todo

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="#">
+          <i className="ri-instagram-fill"></i>
+        </a>
+        <a href="#">
+          <i className="ri-twitter-fill"></i>
+        </a>
+        <a href="#">
+          <i className="ri-facebook-circle-fill"></i>
+        </a>
+        <a href="#">
+          <i className="ri-linkedin-box-fill"></i>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import  './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='navbar'>
+        <a href="#"><i className="ri-home-5-fill"></i></a>
+        <a href="#"> <i className="ri-user-3-fill"></i></a>
+        <a href="#"><i className="ri-phone-fill"></i></a>
+        <a href="#"><i className="ri-chat-3-fill"></i></a>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
